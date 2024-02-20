820225e4b1634430fe3503fdd8b56e31d4e4c190
nafeyah <nafeyahthenx@gmail.com>
2024-02-21 01:01:04 +0530

    updated the repos

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,6 @@
-export const api = "http://localhost:2000/api";
+const baseUrl = process.env.API || "https://rasan-rest-server-31c2cd634a56.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`;
+    return `${baseUrl}/public/${fileName}`;
 };
